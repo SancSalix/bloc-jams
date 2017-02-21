a109552793510543caaafe29a5731598b4f90b62
Shelby <coleshirec@gmail.com>
2017-02-21 17:54:12 -0500

    Events scripting added

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,24 @@
- var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');     
-     var revealPoint = function() {
-         for(var i = 0; i < points.length; i++) {
-             points[i].style.opacity = 1;
-             points[i].style.transform = "scaleX(1) translateY(0)";
-             points[i].style.msTransform = "scaleX(1) translateY(0)";
-             points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-         }
-     };
- 
-     revealPoint();
- 
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";         
+};
+
+var animatePoints = function(points) {      
+    forEach(points, revealPoint);
  };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    window.addEventListener('scroll', function(event) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
+             animatePoints(pointsArray);   
+         }
+     });
+}
+
